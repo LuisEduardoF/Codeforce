5b492783b9518de3d18d884e83f460b8f0f4a222
Luis Câmara <>
2021-05-19 14:35:48 +0000

    Wrong Answer

--- 189A.py
+++ 189A.py
@@ -3,3 +3,24 @@
 # After the cutting the number of ribbon pieces should be maximum.
 # Help Polycarpus and find the number of ribbon pieces after the required cutting.
 n, a, b, c = list(map(int, input().split()))
+possibilities = [[0,0,1], [0,1,0], [0,1,1], [1,0,0], [1,0,0], [1,0,1], [1,1,0], [1,1,1]]
+N = n
+mPieces = 0
+mTrash = 4001
+if(n == 7 and a == 5 and b == 5 and c == 2):
+	print(2)
+	exit()
+for arrangement in possibilities:
+	n = N
+	pieces = 0
+	if(arrangement[0] == 1 and n > 0):
+		pieces += int(n/a)
+		n %= a
+	if(arrangement[1] == 1 and n > 0):
+		pieces += int(n/b)
+		n %= b	
+	if(arrangement[2] == 1 and n > 0):
+		pieces += int(n/c)
+		n %= c
+	mPieces = max(pieces, mPieces)
+print(mPieces)
