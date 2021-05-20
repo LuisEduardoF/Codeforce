fd784610700edea256893c3f6df8e1b0e25204de
Luis Camara <>
2021-05-20 14:19:21 +0000

    Configuration 327A

---
+++ 327A.py
@@ -0,0 +1,18 @@
+def find_0subsequence(board):
+	mSubsequence = 0
+	subsequence = 0
+	nOne = 0
+	for i in range(len(board)):
+		if(board[i] == 0):
+			subsequence += 1
+		else:
+			mSubsequence = max(subsequence, mSubsequence)
+			subsequence = 0
+			nOne += 1
+	if(mSubsequence > 0):
+		return mSubsequence + nOne
+	else:
+		return nOne - 1
+n = int(input())
+board = list(map(int, input().split()))
+print(find_0subsequence(board))
