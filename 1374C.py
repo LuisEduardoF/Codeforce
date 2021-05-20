8d8588847e365c1ea6f4817b01f63df4da88f4a0
Luis Câmara <>
2021-05-20 14:03:04 +0000

    Configurations 1374C

---
+++ 1374C.py
@@ -0,0 +1,21 @@
+def count_brackets(string):
+	left = 0
+	for let in string:
+		if(let == '('):
+			left += 1
+		else:
+			if(left > 0):
+				left -= 1
+	print(left)
+	
+n = int(input())
+for i in range(n):
+	s = int(input())
+	string = input()
+
+
+
+
+
+
+	
